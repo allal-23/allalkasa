e0d38348d29569e626c70c45bca8902a94b1be4a
allal-23 <75438371+allal-23@users.noreply.github.com>
2023-09-14 01:09:02 +0200

    index init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <BrowserRouter>
+      <routes>
+        <Route path="home" element={<Home />}></Route>
+      </routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
